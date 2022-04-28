c70f286c58d987ce92d57fd25f5ded81deb7d1e9
Tyler Hachadorian <49740237+hachadorian@users.noreply.github.com>
2022-04-28 09:09:42 -0400

    pages setup on master

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,7 @@
 module.exports = {
+  pathPrefix: `/MIT`,
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `MIT`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
