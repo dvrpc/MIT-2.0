62685166fe27cbd6989928592db939c0a4bb56da
hachadorian <hachadorian98@gmail.com>
2022-04-28 09:51:43 -0400

    revert

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -7,42 +7,42 @@ const Filter = ({
   setFocusFilter,
   setPrincipleFilter,
 }) => {
-  // const focusAreas = [
-  //   { value: "Communities", label: "Communities", color: "#006ba6" },
-  //   { value: "Environment", label: "Environment", color: "#4fa3a8" },
-  //   {
-  //     value: "Regional Planning",
-  //     label: "Regional Planning",
-  //     color: "#582267",
-  //   },
-  //   { value: "Economy", label: "Economy", color: "#bd2556" },
-  //   { value: "Transportation", label: "Transportation", color: "#b15817" },
-  // ];
+  const focusAreas = [
+    { value: "Communities", label: "Communities", color: "#006ba6" },
+    { value: "Environment", label: "Environment", color: "#4fa3a8" },
+    {
+      value: "Regional Planning",
+      label: "Regional Planning",
+      color: "#582267",
+    },
+    { value: "Economy", label: "Economy", color: "#bd2556" },
+    { value: "Transportation", label: "Transportation", color: "#b15817" },
+  ];
 
-  // const principles = [
-  //   { value: "Equity", label: "Equity", color: "#582267" },
-  //   { value: "Resiliency", label: "Resiliency", color: "#4fa3a8" },
-  //   { value: "Sustainability", label: "Sustainability", color: "#bd2556" },
-  // ];
+  const principles = [
+    { value: "Equity", label: "Equity", color: "#582267" },
+    { value: "Resiliency", label: "Resiliency", color: "#4fa3a8" },
+    { value: "Sustainability", label: "Sustainability", color: "#bd2556" },
+  ];
 
-  // const bgStyles = {
-  //   multiValue: (styles, { data }) => {
-  //     return {
-  //       ...styles,
-  //       backgroundColor: data.color,
-  //     };
-  //   },
-  //   multiValueLabel: (styles, { data }) => ({
-  //     ...styles,
-  //     color: "white",
-  //     fontWeight: "bold",
-  //   }),
-  //   multiValueRemove: (styles, { data }) => ({
-  //     ...styles,
-  //     color: "white",
-  //     ":hover": {},
-  //   }),
-  // };
+  const bgStyles = {
+    multiValue: (styles, { data }) => {
+      return {
+        ...styles,
+        backgroundColor: data.color,
+      };
+    },
+    multiValueLabel: (styles, { data }) => ({
+      ...styles,
+      color: "white",
+      fontWeight: "bold",
+    }),
+    multiValueRemove: (styles, { data }) => ({
+      ...styles,
+      color: "white",
+      ":hover": {},
+    }),
+  };
 
   return (
     <>
@@ -57,29 +57,29 @@ const Filter = ({
       <div className="grid grid-cols-2">
         <div>
           <label>Filter by focus area:</label>
-          {/* <Select
+          <Select
             options={focusAreas}
             isMulti
-            onChange={(selected) => {
+            onChange={selected => {
               const temp = [];
-              selected.map((option) => temp.push(option.value));
+              selected.map(option => temp.push(option.value));
               setFocusFilter([...temp]);
             }}
             styles={bgStyles}
-          /> */}
+          />
         </div>
         <div className="pl-2">
           <label>Filter by principle:</label>
-          {/* <Select
+          <Select
             options={principles}
             isMulti
-            onChange={(selected) => {
+            onChange={selected => {
               const temp = [];
-              selected.map((option) => temp.push(option.value));
+              selected.map(option => temp.push(option.value));
               setPrincipleFilter([...temp]);
             }}
             styles={bgStyles}
-          /> */}
+          />
         </div>
       </div>
     </>
