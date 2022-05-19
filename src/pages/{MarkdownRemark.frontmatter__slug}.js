873406710b21c298f3d6239728d9311dba33a7eb
Tyler Hachadorian <49740237+hachadorian@users.noreply.github.com>
2022-05-19 11:08:57 -0400

    temp formatting

--- src/pages/{MarkdownRemark.frontmatter__slug}.js
+++ src/pages/{MarkdownRemark.frontmatter__slug}.js
@@ -1,42 +1,78 @@
 import React from "react";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
 import Layout from "../components/layout";
-import { useBreadcrumb } from "gatsby-plugin-breadcrumb";
-import BreadCrumb from "../components/BreadCrumb";
 import Logo from "../components/Logo";
+import ToolKit from "../images/toolkit.png";
 
 export default function Template({ data, location }) {
   const { markdownRemark } = data;
   const { frontmatter } = markdownRemark;
-  const { crumbs } = useBreadcrumb({
-    location,
-    crumbLabel: frontmatter.title,
-    crumbSeparator: " / ",
-  });
 
   return (
     <Layout>
-      <BreadCrumb crumbs={crumbs} />
-      <div className="p-12">
-        <div className="text-3xl">{frontmatter.title}...</div>
+      <div className="w-3/4 mt-6 flex flex-col">
         <div className="flex">
-          Focus Areas:
-          {frontmatter.focusAreas.map(focus => {
-            return <Logo focus={focus} />;
+          <div className="text-3xl">{frontmatter.title}</div>
+          <div className="flex items-center ml-auto">
+            {frontmatter.focusAreas.map(focus => {
+              return <Logo focus={focus} />;
+            })}
+          </div>
+        </div>
+        <div className="grid grid-cols-2 mt-8">
+          <div className="w-100 flex justify-center">
+            <img src={ToolKit} alt="toolkit-logo" className="h-[300px] mr-12" />
+          </div>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla
+              aliquet enim tortor at auctor urna. Risus at ultrices mi tempus
+              imperdiet. Ut eu sem integer vitae justo. Convallis a cras semper
+              auctor neque vitae tempus. Nullam vehicula ipsum a arcu cursus
+              vitae congue mauris. Interdum posuere lorem ipsum dolor sit amet
+              consectetur adipiscing elit. Elementum eu facilisis sed odio morbi
+              quis commodo odio. In fermentum posuere urna nec tincidunt
+              praesent. Risus sed vulputate odio ut enim blandit volutpat
+              maecenas volutpat. Mattis nunc sed blandit libero volutpat sed
+              cras ornare arcu. Id semper risus in hendrerit gravida. Vulputate
+              dignissim suspendisse in est ante.
+            </p>
+            <p className="pt-4">
+              Id diam vel quam elementum pulvinar etiam non. Posuere ac ut
+              consequat semper viverra nam libero justo laoreet. Scelerisque
+              felis imperdiet proin fermentum leo vel orci porta. Auctor urna
+              nunc id cursus metus aliquam eleifend. Nibh nisl condimentum id
+              venenatis a condimentum. Donec ac odio tempor orci dapibus
+              ultrices in iaculis. Neque laoreet suspendisse interdum
+              consectetur libero id. Turpis cursus in hac habitasse platea
+              dictumst. Nam aliquam sem et tortor. Aenean sed adipiscing diam
+              donec adipiscing tristique risus. Tellus cras adipiscing enim eu
+              turpis egestas pretium. Libero nunc consequat interdum varius sit.
+              Aliquet nibh praesent tristique magna sit. Sed felis eget velit
+              aliquet sagittis id.
+            </p>
+          </div>
+        </div>
+        <div className="mt-auto">
+          <div>See Also: </div>
+          {frontmatter.seeOther.map(link => {
+            return (
+              <Link
+                className="mr-4 underline"
+                to={
+                  "/" +
+                  link
+                    .toLowerCase()
+                    .replace(/\s/g, "")
+                    .replace(/&|and/g, "-and-")
+                }
+              >
+                {link}
+              </Link>
+            );
           })}
         </div>
-        <div>Principles: </div>
-        {frontmatter.principles.map(principle => {
-          return <div className="ml-4">{principle}</div>;
-        })}
-        <div>See Other: </div>
-        {frontmatter.seeOther.map(link => {
-          return <div className="ml-4">{link}</div>;
-        })}
-        <div>Tracking Progress Links: </div>
-        {frontmatter.trackingProgressLinks.map(link => {
-          return <div className="ml-4">{link}</div>;
-        })}
       </div>
     </Layout>
   );
