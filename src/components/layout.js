ee1b40aa8e939902ccab97a6f59e5f0b7b707a85
hachadorian <hachadorian98@gmail.com>
2022-05-19 13:37:07 -0400

    layout header fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 const Layout = ({ children }) => {
   return (
     <div className="h-screen flex flex-col">
-      <div className="h-[12%] md:h-[8%]" style={{ zIndex: 1000 }}>
+      <div className="h-[12%]" style={{ zIndex: 1000 }}>
         <Header />
       </div>
       <div className="flex justify-center h-[86%] md:h-[84%]">{children}</div>
