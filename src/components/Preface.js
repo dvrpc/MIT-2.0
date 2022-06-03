8424230a0b2b9f5891100be89e13be02554f5e72
Tyler <hachadorian98@gmail.com>
2022-06-03 13:19:37 -0400

    changes from cs

--- src/components/Preface.js
+++ src/components/Preface.js
@@ -37,7 +37,7 @@ const Preface = ({ isVisible, setIsVisible }) => {
   return (
     <div className="flex flex-col w-full" ref={info}>
       <div
-        className="flex flex-col md:flex-row md:py-6 items-center bg-[#edf6f6] justify-center border-b-2 shadow-md min-h-[45vh]"
+        className="flex flex-col md:flex-row md:py-6 bg-[#edf6f6] justify-center border-b-2 shadow-md min-h-[45vh]"
         style={{ display: isVisible ? "" : "none" }}
       >
         <img
@@ -45,18 +45,21 @@ const Preface = ({ isVisible, setIsVisible }) => {
           alt="toolkit-logo"
           className="h-[200px] md:h-[300px] w-auto"
         />
-        <div className="text-lg md:w-3/5 text-sm md:text-base pb-2">
+        <div className="text-lg md:w-1/2 text-sm md:text-base pb-2">
           <div className="md:ml-12">
             <Tabs>
               <TabList className="flex cursor-pointer border-b">
                 <Tab>About</Tab>
-                <Tab>Principles & Focus Areas</Tab>
+                <Tab>Principles</Tab>
+                <Tab>Focus Areas</Tab>
               </TabList>
               <div className="mt-4">
                 <TabPanel>
-                  The Delaware Valley Regional Planning Commission (DVRPC) is
-                  responsible for long-range planning at the regional scale, but
-                  implementation of the Plan—and much of our collective
+                  <span className="preface-bold">
+                    The Delaware Valley Regional Planning Commission (DVRPC)
+                  </span>{" "}
+                  is responsible for long-range planning at the regional scale,
+                  but implementation of the Plan—and much of our collective
                   future—is based on how the region’s 350 municipalities use
                   their home-rule authority to influence land use, provide
                   public services, protect natural and historic resources, and,
@@ -65,9 +68,12 @@ const Preface = ({ isVisible, setIsVisible }) => {
                   municipalities to help implement the goals of{" "}
                   <i>Connections 2050</i>, the region’s Long-Range Plan. It
                   contains resources, case studies, ordinances, and indicators
-                  for nearly 80 tools. Scroll down to find the tools that best
-                  fit your municipality’s needs by using the interactive
-                  filters.
+                  for nearly 80 tools.{" "}
+                  <span className="preface-bold">
+                    Scroll down to find the tools that best fit your
+                    municipality’s needs by using the interactive filters. Lear
+                    more about Principle and Focus Area filters.
+                  </span>
                   <div className="grid grid-cols-4 mt-6 text-center text-[#4fa3a8]">
                     <a
                       className="h-min flex flex-col justify-center"
@@ -113,60 +119,58 @@ const Preface = ({ isVisible, setIsVisible }) => {
                   </div>
                 </TabPanel>
                 <TabPanel>
-                  <div className="flex space-x-4">
-                    <ul className="space-y-2">
-                      <strong>Principles</strong>
-                      <li>
-                        EQUITY: commitment to public outreach and participatory
-                        planning to hear more voices in the development of
-                        communities that we live in, and to increase everyone’s
-                        access to the transportation infrastructure needed for
-                        all of our daily activities
-                      </li>
-                      <li>
-                        SUSTAINABILITY: reduction of emissions to a net zero
-                        level by 2050 through renewable energy, energy
-                        management, the electrification of buildings and
-                        vehicles, and improving transit and non-motorized travel
-                        options
-                      </li>
-                      <li>
-                        RESILIENCY: hardening our communities and their
-                        infrastructure systems or moving them out of harm’s way
-                        in the face of anticipated flooding; ecosystem
-                        degradation; fishing and farming disruptions; and human
-                        health risks.
-                      </li>
-                    </ul>
-                    <ul className="space-y-2">
-                      <strong>Focus Areas</strong>
-                      <li>
-                        <span className="text-[#7b9d3e]">ENVIRONMENT:</span>{" "}
-                        protection and enhancement of natural amenities, air and
-                        water quality, open space, and farmland
-                      </li>
-                      <li>
-                        <span className="text-[#006ba6]">COMMUNITIES:</span>{" "}
-                        inclusive, healthy, and walkable communities that offer
-                        a better quality of life for all residents, using the
-                        more than 135 Plan Centers as a framework for
-                        infrastructure investments
-                      </li>
-                      <li>
-                        <span className="text-[#de6e1d]">
-                          MULTIMODAL TRANSPORTATION:
-                        </span>{" "}
-                        safe, healthy, and environmentally friendly
-                        transportation accessible to a wide range of users for a
-                        variety of trip purposes
-                      </li>
-                      <li>
-                        <span className="text-[#bd2556]">ECONOMY:</span>{" "}
-                        accelerating the transition to a more environmentally
-                        friendly economy, enhancing workforce skills, and
-                        supporting inclusive growth.
-                      </li>
-                    </ul>
+                  <div className="grid grid-cols-2">
+                    <div>
+                      <span className="preface-bold">EQUITY:</span> commitment
+                      to public outreach and participatory planning to hear more
+                      voices in the development of communities that we live in,
+                      and to increase everyone’s access to the transportation
+                      infrastructure needed for all of our daily activities
+                    </div>
+                    <div>
+                      <span className="preface-bold">SUSTAINABILITY:</span>{" "}
+                      reduction of emissions to a net zero level by 2050 through
+                      renewable energy, energy management, the electrification
+                      of buildings and vehicles, and improving transit and
+                      non-motorized travel options
+                    </div>
+                    <div className="mt-4">
+                      <span className="preface-bold">RESILIENCY:</span>{" "}
+                      hardening our communities and their infrastructure systems
+                      or moving them out of harm’s way in the face of
+                      anticipated flooding; ecosystem degradation; fishing and
+                      farming disruptions; and human health risks.
+                    </div>
+                  </div>
+                </TabPanel>
+                <TabPanel>
+                  <div className="grid grid-cols-2">
+                    <div>
+                      <span className="preface-bold">
+                        MULTIMODAL TRANSPORTATION:
+                      </span>{" "}
+                      safe, healthy, and environmentally friendly transportation
+                      accessible to a wide range of users for a variety of trip
+                      purposes
+                    </div>
+                    <div>
+                      <span className="preface-bold">COMMUNITIES:</span>{" "}
+                      inclusive, healthy, and walkable communities that offer a
+                      better quality of life for all residents, using the more
+                      than 135 Plan Centers as a framework for infrastructure
+                      investments
+                    </div>
+                    <div className="mt-4">
+                      <span className="preface-bold">ECONOMY:</span>{" "}
+                      accelerating the transition to a more environmentally
+                      friendly economy, enhancing workforce skills, and
+                      supporting inclusive growth.
+                    </div>
+                    <div className="mt-4">
+                      <span className="preface-bold">ENVIRONMENT:</span>{" "}
+                      protection and enhancement of natural amenities, air and
+                      water quality, open space, and farmland
+                    </div>
                   </div>
                 </TabPanel>
               </div>
