de0650a1399c8df8911ea319993d1e1a186dbec6
Tyler Hachadorian <49740237+hachadorian@users.noreply.github.com>
2022-10-13 16:13:49 -0400

    optimize image loading

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,19 @@ module.exports = {
         path: `${__dirname}/src/markdown-pages`,
       },
     },
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 800,
+            },
+          },
+        ],
+      },
+    },
     `gatsby-plugin-breadcrumb`,
   ],
 };
