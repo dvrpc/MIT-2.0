3da8dbad7c52930df65d478e924653d28e3dc2c5
hachadorian <hachadorian98@gmail.com>
2023-05-16 15:59:39 -0400

    add export

--- src/utils/trackingProgressLinks.js
+++ src/utils/trackingProgressLinks.js
@@ -1,25 +1,40 @@
 const trackingProgressLinks = {
-	'Air Quality': 'https://www.dvrpc.org/TrackingProgress/#Air-Quality/',
-	'Bridge Conditions': 'https://www.dvrpc.org/TrackingProgress/#Bridge-Conditions/',
-	'Business Formations': 'https://www.dvrpc.org/TrackingProgress/#Business-Formations/',
-	'Community Integration': 'https://www.dvrpc.org/TrackingProgress/#Community-Integration/',
-	'Commute Mode': 'https://www.dvrpc.org/TrackingProgress/#Commute-Mode/',
-	Congestion: 'https://www.dvrpc.org/TrackingProgress/#Congestion/',
-	'Educational Attainment': 'https://www.dvrpc.org/TrackingProgress/#Educational-Attainment/',
-	'Greenhouse Gas Emissions': 'https://www.dvrpc.org/TrackingProgress/#Greenhouse-Gas-Emissions/',
-	'Gross Domestic Product': 'https://www.dvrpc.org/TrackingProgress/#Gross-Domestic-Product/',
-	'Housing Affordability': 'https://www.dvrpc.org/TrackingProgress/#Housing-Affordability/',
-	'Housing Permits': 'https://www.dvrpc.org/TrackingProgress/#Housing-Permits/',
-	Income: 'https://www.dvrpc.org/TrackingProgress/#Income/',
-	'Job Growth': 'https://www.dvrpc.org/TrackingProgress/#Job-Growth/',
-	'Labor Force': 'https://www.dvrpc.org/TrackingProgress/#Labor-Force/',
-	'Land Consumption': 'https://www.dvrpc.org/TrackingProgress/#Land-Consumption/',
-	'Miles Driven': 'https://www.dvrpc.org/TrackingProgress/#Miles-Driven/',
-	'Mortgage Lending': 'https://www.dvrpc.org/TrackingProgress/#Mortgage-Lending/',
-	'Pavement Conditions': 'https://www.dvrpc.org/TrackingProgress/#Pavement-Conditions/',
-	'Population Growth': 'https://www.dvrpc.org/TrackingProgress/#Population-Growth/',
-	'Roadway Safety': 'https://www.dvrpc.org/TrackingProgress/#Roadway-Safety/',
-	'Transit Conditions': 'https://www.dvrpc.org/TrackingProgress/#Transit-Conditions/',
-	'Transit Ridership': 'https://www.dvrpc.org/TrackingProgress/#Transit-Ridership/',
-	'Water Quality': 'https://www.dvrpc.org/TrackingProgress/#Water-Quality/'
-}
+  "Air Quality": "https://www.dvrpc.org/TrackingProgress/#Air-Quality/",
+  "Bridge Conditions":
+    "https://www.dvrpc.org/TrackingProgress/#Bridge-Conditions/",
+  "Business Formations":
+    "https://www.dvrpc.org/TrackingProgress/#Business-Formations/",
+  "Community Integration":
+    "https://www.dvrpc.org/TrackingProgress/#Community-Integration/",
+  "Commute Mode": "https://www.dvrpc.org/TrackingProgress/#Commute-Mode/",
+  Congestion: "https://www.dvrpc.org/TrackingProgress/#Congestion/",
+  "Educational Attainment":
+    "https://www.dvrpc.org/TrackingProgress/#Educational-Attainment/",
+  "Greenhouse Gas Emissions":
+    "https://www.dvrpc.org/TrackingProgress/#Greenhouse-Gas-Emissions/",
+  "Gross Domestic Product":
+    "https://www.dvrpc.org/TrackingProgress/#Gross-Domestic-Product/",
+  "Housing Affordability":
+    "https://www.dvrpc.org/TrackingProgress/#Housing-Affordability/",
+  "Housing Permits": "https://www.dvrpc.org/TrackingProgress/#Housing-Permits/",
+  Income: "https://www.dvrpc.org/TrackingProgress/#Income/",
+  "Job Growth": "https://www.dvrpc.org/TrackingProgress/#Job-Growth/",
+  "Labor Force": "https://www.dvrpc.org/TrackingProgress/#Labor-Force/",
+  "Land Consumption":
+    "https://www.dvrpc.org/TrackingProgress/#Land-Consumption/",
+  "Miles Driven": "https://www.dvrpc.org/TrackingProgress/#Miles-Driven/",
+  "Mortgage Lending":
+    "https://www.dvrpc.org/TrackingProgress/#Mortgage-Lending/",
+  "Pavement Conditions":
+    "https://www.dvrpc.org/TrackingProgress/#Pavement-Conditions/",
+  "Population Growth":
+    "https://www.dvrpc.org/TrackingProgress/#Population-Growth/",
+  "Roadway Safety": "https://www.dvrpc.org/TrackingProgress/#Roadway-Safety/",
+  "Transit Conditions":
+    "https://www.dvrpc.org/TrackingProgress/#Transit-Conditions/",
+  "Transit Ridership":
+    "https://www.dvrpc.org/TrackingProgress/#Transit-Ridership/",
+  "Water Quality": "https://www.dvrpc.org/TrackingProgress/#Water-Quality/",
+};
+
+export default trackingProgressLinks;
