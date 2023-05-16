7ca97dba0bd30eb83949fd9252c8745a7785ec6f
hachadorian <hachadorian98@gmail.com>
2023-05-16 15:53:25 -0400

    updated tp links

--- src/utils/trackingProgressLinks.js
+++ src/utils/trackingProgressLinks.js
@@ -1,46 +1,25 @@
 const trackingProgressLinks = {
-  "Air Quality": "https://www.dvrpc.org/TrackingProgress/#Air-Quality/enviro",
-  "Bridge Conditions":
-    "https://www.dvrpc.org/TrackingProgress/#Bridge-Conditions/transpo",
-  "Commute Mode":
-    "https://www.dvrpc.org/TrackingProgress/#Commute-Mode/transpo",
-  Congestion: "https://www.dvrpc.org/TrackingProgress/#Congestion/transpo",
-  "Educational Attainment":
-    "https://www.dvrpc.org/TrackingProgress/#Educational-Attainment/comm",
-  "Exported Goods":
-    "https://www.dvrpc.org/TrackingProgress/#Exported-Goods/econo",
-  "Global Connectivity":
-    "https://www.dvrpc.org/TrackingProgress/#Global-Connectivity/econo",
-  "Greenhouse Gas Emissions":
-    "https://www.dvrpc.org/TrackingProgress/#Greenhouse-Gas-Emissions/enviro",
-  "Housing Activity":
-    "https://www.dvrpc.org/TrackingProgress/#Housing-Activity/comm",
-  "Housing Affordability":
-    "https://www.dvrpc.org/TrackingProgress/#Housing-Affordability/comm",
-  "Income Disparities":
-    "https://www.dvrpc.org/TrackingProgress/#Income-Disparities/equity",
-  Innovation: "https://www.dvrpc.org/TrackingProgress/#Innovation/econo",
-  "Job Growth": "https://www.dvrpc.org/TrackingProgress/#Job-Growth/econo",
-  "Land Consumption":
-    "https://www.dvrpc.org/TrackingProgress/#Land-Consumption/enviro",
-  "Miles Driven":
-    "https://www.dvrpc.org/TrackingProgress/#Miles-Driven/transpo",
-  "Pavement Conditions":
-    "https://www.dvrpc.org/TrackingProgress/#Pavement-Conditions/transpo",
-  "Population Growth":
-    "https://www.dvrpc.org/TrackingProgress/#Population-Growth/comm",
-  "Racial & Ethnic Disparities":
-    "https://www.dvrpc.org/TrackingProgress/#Racial-&-Ethnic-Disparities/equity",
-  "Roadway Safety":
-    "https://www.dvrpc.org/TrackingProgress/#Roadway-Safety/transpo",
-  "Sex Disparities":
-    "https://www.dvrpc.org/TrackingProgress/#Sex-Disparities/equity",
-  "Transit Conditions":
-    "https://www.dvrpc.org/TrackingProgress/#Transit-Conditions/transpo",
-  "Transit Ridership":
-    "https://www.dvrpc.org/TrackingProgress/#Transit-Ridership/transpo",
-  "Water Quality":
-    "https://www.dvrpc.org/TrackingProgress/#Water-Quality/enviro",
-};
-
-export default trackingProgressLinks;
+	'Air Quality': 'https://www.dvrpc.org/TrackingProgress/#Air-Quality/',
+	'Bridge Conditions': 'https://www.dvrpc.org/TrackingProgress/#Bridge-Conditions/',
+	'Business Formations': 'https://www.dvrpc.org/TrackingProgress/#Business-Formations/',
+	'Community Integration': 'https://www.dvrpc.org/TrackingProgress/#Community-Integration/',
+	'Commute Mode': 'https://www.dvrpc.org/TrackingProgress/#Commute-Mode/',
+	Congestion: 'https://www.dvrpc.org/TrackingProgress/#Congestion/',
+	'Educational Attainment': 'https://www.dvrpc.org/TrackingProgress/#Educational-Attainment/',
+	'Greenhouse Gas Emissions': 'https://www.dvrpc.org/TrackingProgress/#Greenhouse-Gas-Emissions/',
+	'Gross Domestic Product': 'https://www.dvrpc.org/TrackingProgress/#Gross-Domestic-Product/',
+	'Housing Affordability': 'https://www.dvrpc.org/TrackingProgress/#Housing-Affordability/',
+	'Housing Permits': 'https://www.dvrpc.org/TrackingProgress/#Housing-Permits/',
+	Income: 'https://www.dvrpc.org/TrackingProgress/#Income/',
+	'Job Growth': 'https://www.dvrpc.org/TrackingProgress/#Job-Growth/',
+	'Labor Force': 'https://www.dvrpc.org/TrackingProgress/#Labor-Force/',
+	'Land Consumption': 'https://www.dvrpc.org/TrackingProgress/#Land-Consumption/',
+	'Miles Driven': 'https://www.dvrpc.org/TrackingProgress/#Miles-Driven/',
+	'Mortgage Lending': 'https://www.dvrpc.org/TrackingProgress/#Mortgage-Lending/',
+	'Pavement Conditions': 'https://www.dvrpc.org/TrackingProgress/#Pavement-Conditions/',
+	'Population Growth': 'https://www.dvrpc.org/TrackingProgress/#Population-Growth/',
+	'Roadway Safety': 'https://www.dvrpc.org/TrackingProgress/#Roadway-Safety/',
+	'Transit Conditions': 'https://www.dvrpc.org/TrackingProgress/#Transit-Conditions/',
+	'Transit Ridership': 'https://www.dvrpc.org/TrackingProgress/#Transit-Ridership/',
+	'Water Quality': 'https://www.dvrpc.org/TrackingProgress/#Water-Quality/'
+}
